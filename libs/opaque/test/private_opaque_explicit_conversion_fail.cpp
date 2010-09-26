f7597604f2fe317969a4e347b7bade21ea71aab5
viboes <viboes@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-09-26 14:59:44 +0000

    Opaque: Check explicit conversion from private OT to UT
    git-svn-id: http://svn.boost.org/svn/boost/sandbox/opaque@65609 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ libs/opaque/test/private_opaque_explicit_conversion_fail.cpp
@@ -0,0 +1,38 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// (C) Copyright Vicente J. Botet Escriba 2010. Distributed under the Boost
+// Software License, Version 1.0. (See accompanying file
+// LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// See http://www.boost.org/libs/opaque for documentation.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#include <boost/opaque/opaque.hpp>
+
+#include <boost/test/unit_test.hpp>
+
+using namespace boost;
+using namespace boost::unit_test;
+
+
+BOOST_OPAQUE_PRIVATE_TYPEDEF(int,A);
+
+template <typename T> void f(T v) {}
+
+void public_multiple_levels_fail() {
+    
+    A a;
+ #if !defined(BOOST_NO_EXPLICIT_CONVERSION_OPERATORS)
+    // explicit conversion from OT to UT works in this case so make compile fail
+#error `struct A' used where a `int' was expected
+#else
+    int i;
+    i=int(a);
+    f(i);
+    //error: `OT' used where a `UT' was expected
+#endif
+
+}
+
+
