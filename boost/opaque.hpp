875d4a5fdc5128f7ec1a95a601ea72279270f395
viboes <viboes@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-09-30 20:36:03 +0000

    Opaque: * Add header at the boost level
    git-svn-id: http://svn.boost.org/svn/boost/sandbox/opaque@65700 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ boost/opaque.hpp
@@ -0,0 +1,20 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// (C) Copyright Vicente J. Botet Escriba 2010.
+// Distributed under the Boost
+// Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or
+// copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// See http://www.boost.org/libs/opaque for documentation.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef BOOST_OPAQUE__HPP
+#define BOOST_OPAQUE__HPP
+
+#include <boost/config.hpp>
+#include <boost/opaque/opaque.hpp>
+
+
+#endif 
