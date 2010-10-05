6e8c9f202188c76bcafbb349bb32e1242ac70bd4
viboes <viboes@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-10-05 21:15:27 +0000

    Opaque: * Update Jamfile and add more tests
    git-svn-id: http://svn.boost.org/svn/boost/sandbox/opaque@65775 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ libs/opaque/test/new_class/hiding_plus_fail.cpp
@@ -0,0 +1,54 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// (C) Copyright Vicente J. Botet Escriba 2010. Distributed under the Boost
+// Software License, Version 1.0. (See accompanying file
+// LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// See http://www.boost.org/libs/opaque for documentation.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#include <boost/opaque/opaque.hpp>
+
+#include <boost/test/unit_test.hpp>
+
+using namespace boost;
+
+typedef int UT;
+typedef short UT2;
+
+// NEW_CLASS(NT,UT,((opaque::using_equality_comparable1<>)))
+struct NT : 
+    boost::new_class<NT, UT
+    , boost::mpl::vector<
+		boost::opaque::using_plus, 
+		boost::opaque::hiding_plus 
+    >
+    >
+{
+    typedef 
+    boost::new_class<NT, UT
+    , boost::mpl::vector<
+	boost::opaque::using_plus, 
+	boost::opaque::hiding_plus 
+    >
+    >
+    base_type;
+    
+    NT(){} 
+    explicit NT(unsigned v) : base_type(v) {}
+    template <typename W> 
+    explicit NT(W w) 
+        : base_type(w) 
+    {}
+    NT(NT const& r) 
+        : base_type(r.val_) 
+    {}
+};
+
+
+void fail() {
+    NT a1(1), b2(2);
+    NT c = (a1 + b2);
+}
+
---
+++ libs/opaque/test/new_class/multiply_assign_fail.cpp
@@ -0,0 +1,40 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// (C) Copyright Vicente J. Botet Escriba 2010. Distributed under the Boost
+// Software License, Version 1.0. (See accompanying file
+// LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// See http://www.boost.org/libs/opaque for documentation.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#include <boost/opaque/opaque.hpp>
+
+using namespace boost;
+
+typedef unsigned UT;
+
+// NEW_CLASS(NT1,UT)
+struct NT1 : boost::new_class<NT1, UT> 
+{
+    typedef 
+    boost::new_class<NT1, UT>
+    base_type;
+    
+    NT1(){} 
+    explicit NT1(unsigned v) : base_type(v) {}
+    template <typename W> 
+    explicit NT1(W w) 
+        : base_type(w) 
+    {}
+    NT1(NT1 const& r) 
+        : base_type(r.val_) 
+    {}
+};
+
+void fail() {
+	NT1 a1(1), b2(2);
+	a1 *= b2;
+}
+
+
