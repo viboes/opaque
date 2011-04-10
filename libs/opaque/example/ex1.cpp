d0395be868e13d797e922a8f3c7ac3515d527a50
viboes <viboes@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-04-10 20:28:26 +0000

    Conversion: cleanup
    git-svn-id: http://svn.boost.org/svn/boost/sandbox/opaque@71169 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/opaque/example/ex1.cpp
+++ libs/opaque/example/ex1.cpp
@@ -22,22 +22,22 @@ bool gt(Int a, Int b)
 {
     if(a) return a > b;  // 1 don't fails even if a is not a bool
     else return 0;       // 2 don't fails even if 0 is not a bool
-} 
+}
 #else
 struct private_unsigned_tag;
 typedef boost::opaque::new_type<unsigned, private_unsigned_tag, boost::mpl::vector<
-    opaque::using_totally_ordered1<opaque::boolean> 
+    opaque::using_totally_ordered<opaque::boolean>
 > > Int;
-//~ BOOST_OPAQUE_PRIVATE_TYPEDEF(int, Int);
+//~ BOOST_OPAQUE_NEW_TYPE(int, Int,opaque::using_totally_ordered1<opaque::boolean>);
 
 opaque::boolean gt(Int a, Int b)
 {
     if(a!=Int(0)) return a > b;  // 1
     else return opaque::false_;       // 2
-} 
+}
 #endif
 
-    
+
 void t() {
     BOOST_TEST(gt(Int(1),Int(0)));
 }
